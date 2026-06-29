474daa46fafdaba39e165c4ddbf01ede8cbb57b7
Hammad Ali Haider <hammadhaideerr@gmail.com>
2026-06-29 22:31:27 +0800

    fix(visa_solver): replace f-string paths with os.path.join, modernise class syntax

--- scripts/visa_solver.py
+++ scripts/visa_solver.py
@@ -1,35 +1,46 @@
-import os
 import json
+import os
 
 
-class VisASolver(object):
+class VisASolver:
     CLSNAMES = [
         'candle', 'capsules', 'cashew', 'chewinggum', 'fryum',
         'macaroni1', 'macaroni2', 'pcb1', 'pcb2', 'pcb3',
         'pcb4', 'pipe_fryum',
     ]
 
-    def __init__(self, root='./VisA_pytorch/1cls'):
+    def __init__(self, root='./data/VisA_pytorch/1cls'):
         self.root = root
-        self.meta_path = f'{root}/meta.json'
+        self.meta_path = os.path.join(root, 'meta.json')
 
     def run(self):
         info = dict(train={}, test={})
         for cls_name in self.CLSNAMES:
-            cls_dir = f'{self.root}/{cls_name}'
+            cls_dir = os.path.join(self.root, cls_name)
             for phase in ['train', 'test']:
                 cls_info = []
-                species = os.listdir(f'{cls_dir}/{phase}')
+                species = os.listdir(os.path.join(cls_dir, phase))
                 for specie in species:
-                    is_abnormal = True if specie not in ['good'] else False
-                    img_names = os.listdir(f'{cls_dir}/{phase}/{specie}')
-                    mask_names = os.listdir(f'{cls_dir}/ground_truth/{specie}') if is_abnormal else None
+                    is_abnormal = specie not in ['good']
+                    img_names = os.listdir(os.path.join(cls_dir, phase, specie))
+                    mask_names = (
+                        os.listdir(os.path.join(cls_dir, 'ground_truth', specie))
+                        if is_abnormal
+                        else None
+                    )
                     img_names.sort()
-                    mask_names.sort() if mask_names is not None else None
+                    if mask_names is not None:
+                        mask_names.sort()
                     for idx, img_name in enumerate(img_names):
                         info_img = dict(
-                            img_path=f'{cls_name}/{phase}/{specie}/{img_name}',
-                            mask_path=f'{cls_name}/ground_truth/{specie}/{mask_names[idx]}' if is_abnormal else '',
+                            img_path=os.path.join(cls_name, phase, specie, img_name),
+                            mask_path=(
+                                os.path.join(
+                                    cls_name, 'ground_truth', specie, mask_names[idx]
+                                )
+                                if is_abnormal
+                                else ''
+                            ),
                             cls_name=cls_name,
                             specie_name=specie,
                             anomaly=1 if is_abnormal else 0,
@@ -38,6 +49,7 @@ class VisASolver(object):
                 info[phase][cls_name] = cls_info
         with open(self.meta_path, 'w') as f:
             f.write(json.dumps(info, indent=4) + "\n")
+
 
 if __name__ == '__main__':
     runner = VisASolver(root='./data/VisA_pytorch/1cls')
