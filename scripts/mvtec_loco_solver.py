0c412d939bdbc45b31a93d11c90ac1dd6596b4de
Hammad Ali Haider <hammadhaideerr@gmail.com>
2026-06-30 22:54:21 +0800

    fix(mvtec_loco_solver): replace f-string paths with os.path.join, remove dead comments

--- scripts/mvtec_loco_solver.py
+++ scripts/mvtec_loco_solver.py
@@ -1,43 +1,58 @@
-import os
 import json
+import os
 
 
-class MVTecLOCOSolver(object):
+class MVTecLOCOSolver:
     CLSNAMES = [
-        'breakfast_box', 'juice_bottle', 'pushpins', 'screw_bag', 'splicing_connectors'
+        'breakfast_box', 'juice_bottle', 'pushpins', 'screw_bag', 'splicing_connectors',
     ]
 
-    def __init__(self, root='data/mvtec'):
+    def __init__(self, root='./data/mvtec_loco_caption'):
         self.root = root
-        self.meta_path = f'{root}/meta.json'
+        self.meta_path = os.path.join(root, 'meta.json')
 
     def run(self):
         info = dict(train={}, test={})
         for cls_name in self.CLSNAMES:
-            cls_dir = f'{self.root}/{cls_name}'
+            cls_dir = os.path.join(self.root, cls_name)
             for phase in ['train', 'test']:
                 cls_info = []
-                species = os.listdir(f'{cls_dir}/{phase}')
+                species = os.listdir(os.path.join(cls_dir, phase))
                 for specie in species:
-                    is_abnormal = True if specie not in ['good'] else False
-                    img_names = os.listdir(f'{cls_dir}/{phase}/{specie}')
-                    mask_names = os.listdir(f'{cls_dir}/ground_truth_merge_mask/{specie}_merge_mask') if is_abnormal else None
+                    is_abnormal = specie not in ['good']
+                    img_names = os.listdir(os.path.join(cls_dir, phase, specie))
+                    mask_names = (
+                        os.listdir(
+                            os.path.join(
+                                cls_dir, 'ground_truth_merge_mask', f'{specie}_merge_mask'
+                            )
+                        )
+                        if is_abnormal
+                        else None
+                    )
                     img_names.sort()
-                    mask_names.sort() if mask_names is not None else None
+                    if mask_names is not None:
+                        mask_names.sort()
                     for idx, img_name in enumerate(img_names):
                         info_img = dict(
-                            img_path=f'{cls_name}/{phase}/{specie}/{img_name}',
-                            mask_path=f'{cls_name}/ground_truth_merge_mask/{specie}_merge_mask/{mask_names[idx]}' if is_abnormal else '',
+                            img_path=os.path.join(cls_name, phase, specie, img_name),
+                            mask_path=(
+                                os.path.join(
+                                    cls_name, 'ground_truth_merge_mask',
+                                    f'{specie}_merge_mask', mask_names[idx],
+                                )
+                                if is_abnormal
+                                else ''
+                            ),
                             cls_name=cls_name,
                             specie_name=specie,
                             anomaly=1 if is_abnormal else 0,
                         )
-                        # print(specie)
-                        # if specie != 'structural_anomalies':
                         cls_info.append(info_img)
                 info[phase][cls_name] = cls_info
         with open(self.meta_path, 'w') as f:
             f.write(json.dumps(info, indent=4) + "\n")
+
 
 if __name__ == '__main__':
     runner = MVTecLOCOSolver(root='./data/mvtec_loco_caption')
